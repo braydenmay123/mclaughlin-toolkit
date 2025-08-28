cada7601f159b6effcb9b0e1b697663f5d5e2cab
braydenmay123 <braydenmay123@gmail.com>
2025-08-27 20:02:08 -0400

    Update app.config.js

--- app.config.js
+++ app.config.js
@@ -14,42 +14,39 @@ module.exports = {
     splash: {
       image: "./assets/images/splash-icon.png",
       resizeMode: "contain",
-      backgroundColor: "#ffffff",
+      backgroundColor: "#ffffff"
     },
 
     ios: {
       supportsTablet: true,
-      bundleIdentifier: "app.rork.home-buyers-planning-tool",
+      bundleIdentifier: "app.rork.home-buyers-planning-tool"
     },
 
     android: {
       adaptiveIcon: {
         foregroundImage: "./assets/images/adaptive-icon.png",
-        backgroundColor: "#ffffff",
+        backgroundColor: "#ffffff"
       },
-      package: "app.rork.home-buyers-planning-tool",
+      package: "app.rork.home-buyers-planning-tool"
     },
 
     web: {
       bundler: "webpack",
       output: "static",
       favicon: "./assets/images/favicon.png",
-      // CSP relaxed enough for Expo web; we can tighten later if desired.
       contentSecurityPolicy:
-        "default-src 'self'; script-src 'self' 'unsafe-eval' 'wasm-unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data: blob:; connect-src *; font-src 'self' data:; worker-src 'self' blob:",
+        "default-src 'self'; script-src 'self' 'unsafe-eval' 'wasm-unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data: blob:; connect-src *; font-src 'self' data:; worker-src 'self' blob:"
     },
 
     plugins: [
       [
         "expo-router",
-        {
-          origin: "https://rork.com/",
-        },
-      ],
+        { origin: "https://rork.com/" }
+      ]
     ],
 
     experiments: {
-      typedRoutes: true,
-    },
-  },
+      typedRoutes: true
+    }
+  }
 };
